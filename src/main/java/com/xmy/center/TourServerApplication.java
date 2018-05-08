9cfb0de583cf096b1505a021bf7b8df39e902e12
xu <770212572@qq.com>
2018-05-08 20:11:09 +0800

    xuanbing

--- src/main/java/com/xmy/center/TourServerApplication.java
+++ src/main/java/com/xmy/center/TourServerApplication.java
@@ -1,4 +1,4 @@
-package com.xmy.tourserver;
+package com.xmy.center;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/xmy/center/TourServerApplicationTests.java
+++ src/test/java/com/xmy/center/TourServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.xmy.tourserver;
+package com.xmy.center;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
